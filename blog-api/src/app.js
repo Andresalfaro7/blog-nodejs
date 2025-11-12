896f56aed4375a0a67efff4e834f446a3faf6577
Andresalfaro7 <andresalfaroparada@gmail.com>
2025-11-12 16:42:40 -0600

    Add module of the subcategories

--- blog-api/src/app.js
+++ blog-api/src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 import categoryRoutes from "./routes/category.routes.js";
+import subcategoryRoutes from "./routes/subcategory.routes.js";
 
 const app = express();
 
-app.use(cors({origin: '*'}));
+app.use(cors({origin: 'http://127.0.0.1:5500'}));
 app.use(express.json());
 app.use('/api/usuarios', userRoutes);
 app.use('/api/categorias', categoryRoutes);
+app.use('/api/subcategorias', subcategoryRoutes);
 
 export default app;
