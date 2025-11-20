e1eb7e7f701ca7107cd562de1530a90f6de77e9e
Andresalfaro7 <andresalfaroparada@gmail.com>
2025-11-20 16:00:28 -0600

    Add validate data of categories, subcategories and users

--- blog-api/src/routes/user.routes.js
+++ blog-api/src/routes/user.routes.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { getUsers, getUser, addUser, editUser, removeUser } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/upload.middleware.js';
 import { validateSchema } from '../middlewares/validator.middleware.js';
+import { createUserSchema } from '../schemas/user.schema.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
-router.post('/', upload.none(), addUser);
+router.post('/', upload.none(), validateSchema(createUserSchema), addUser);
 router.put('/:id', editUser);
 router.delete('/:id', removeUser);
 
--- blog-api/src/services/user.service.js
+++ blog-api/src/services/user.service.js
@@ -4,3 +4,8 @@ export const existUser = async (id) => {
   const [rows] = await pool.query('SELECT id FROM usuarios WHERE id = ?', [id]);
   return rows[0] || null;
 };
+
+export const getUserByEmail = async (email) =>{
+  const [rows] = await pool.query('SELECT email FROM usuarios WHERE email = ?', [email]);
+  return rows[0] || null;
+}
