f333078c3282b7b4f4667929741db667ef16161c
Andresalfaro7 <andresalfaroparada@gmail.com>
2025-11-25 10:11:05 -0600

    Add first functionality of hte precess jwt authentication

---
+++ blog-api/src/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { verifyPassword } from "../services/user.service.js";
+import { saveToken, deleteToken } from "../models/token.model.js";
+
+export const login = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        console.log("email", email);
+
+        const user = await verifyPassword(email, password);
+        if (!user) return res.status(401).json({ message: "Verifique sus credenciales" });
+
+        // Crear token
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES }
+        );
+        console.log("token", token);
+        console.log("user", user);
+
+        await saveToken(user.id, token);
+
+        return res.json({ 
+            message: "Inicio de sesión exitoso",
+            token: token 
+        });
+    } catch (error) {
+        return res
+        .status(500)
+        .json({ 
+            message: "Ocurrió un error, por favor intenta de nuevo más tarde.",
+            error: error 
+        });
+    }
+};
